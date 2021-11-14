4c461a9d84261423fc550e6c63b81549f9284eb4
danerop <danerop@hotmail.com>
2021-11-14 17:49:55 -0300

    mejoras en el header, busqueda, idioma, usuario

--- js/code.js
+++ js/code.js
@@ -1,24 +1,39 @@
 $(document).ready(function(){
         
     //activa/desactiva la barra de busqueda.
-    $(".busqueda").click(function(){
-      if( !$(".navegacion-principal ul").hasClass("hide") ){
-        $(".navegacion-principal ul").addClass("hide");
-        $(".busqueda-activada").removeClass("hide");
-      }
-      $("body>div, main, footer").click(function(){
-        $(".navegacion-principal ul").removeClass("hide");
-        $(".busqueda-activada").addClass("hide");
-      });
+  $(".busqueda").click(function(){
+    if( !$(".navegacion-principal ul").hasClass("hide") ){
+      $(".navegacion-principal ul").addClass("hide");
+      $(".busqueda-activada").removeClass("hide");
+    }
+    $("body>div, main, footer").click(function(){
+      $(".navegacion-principal ul").removeClass("hide");
+      $(".busqueda-activada").addClass("hide");
     });
+  });
 
     //activa/desactiva el selector de idioma
-    $("#selector-lenguaje").click(function(){
-      if( $(".lenguaje-contenedor").hasClass("hide") ){
-        $(".lenguaje-contenedor").removeClass("hide");
-      }else{
-        $(".lenguaje-contenedor").addClass("hide");
+  $("#selector-lenguaje").click(function(){
+    if( $(".lenguaje-contenedor").hasClass("hide") ){
+      $(".lenguaje-contenedor").removeClass("hide");
+    }else{
+      $(".lenguaje-contenedor").addClass("hide");
+    }
+  });
+
+    //activa/desactiva el cuadro de usuario
+  $(".control-usuario").click(function(){
+    if( $(".usuario-contenedor").hasClass("hide") ){
+      $(".usuario-contenedor").removeClass("hide");
+    }else{
+      $(".usuario-contenedor").addClass("hide");
+    }
+
+    $("body>div, main, footer").click(function(){
+      if( !$(".usuario-contenedor").hasClass("hide") ){
+        $(".usuario-contenedor").addClass("hide");
       }
     });
-
   });
+
+});
