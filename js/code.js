285159d628438ab6a6504282055ea5708a7b305e
danerop <danerop@hotmail.com>
2021-11-19 21:09:03 -0300

    responsive header, botones crea-tu-calzado y correcciones menores

--- js/code.js
+++ js/code.js
@@ -1,19 +1,15 @@
 $(document).ready(function(){
         
-    //activa/desactiva la barra de busqueda.
+    //activa la barra de busqueda.
   $(".busqueda").click(function(){
     if( !$(".navegacion-principal ul").hasClass("hide") ){
       $(".navegacion-principal ul").addClass("hide");
       $(".busqueda-activada").removeClass("hide");
     }
-    $("body>div, main, footer").click(function(){
-      $(".navegacion-principal ul").removeClass("hide");
-      $(".busqueda-activada").addClass("hide");
-    });
   });
 
     //activa/desactiva el selector de idioma
-  $("#selector-lenguaje").click(function(){
+  $(".selector-lenguaje").click(function(){
     if( $(".lenguaje-contenedor").hasClass("hide") ){
       $(".lenguaje-contenedor").removeClass("hide");
     }else{
@@ -28,12 +24,49 @@ $(document).ready(function(){
     }else{
       $(".usuario-contenedor").addClass("hide");
     }
-
-    $("body>div, main, footer").click(function(){
-      if( !$(".usuario-contenedor").hasClass("hide") ){
-        $(".usuario-contenedor").addClass("hide");
-      }
-    });
   });
 
+    //desactiva a los tres de arriba al dar click afuera.
+  $("body>div, main, footer").click(function(){
+    if( !$(".usuario-contenedor").hasClass("hide") ){
+      $(".usuario-contenedor").addClass("hide");
+    }
+    if( $(".navegacion-principal ul").hasClass("hide") ){
+      $(".navegacion-principal ul").removeClass("hide");
+      $(".busqueda-activada").addClass("hide");
+    }
+    if( !$(".lenguaje-contenedor").hasClass("hide") ){
+      $(".lenguaje-contenedor").addClass("hide");
+    }
+  });
+
+
+  //CREA TU CALZADO
+  //activa los cuadros de opciones de crea tu calzado
+  $(".bl-izquierda.boton-edit-calzado").click(function(){
+    $(".bl-izquierda").removeClass("hide");
+    $(".bl-izquierda.boton-edit-calzado").addClass("hide");
+  });
+  $(".bh-inferior.boton-edit-calzado").click(function(){
+    $(".bh-inferior").removeClass("hide");
+    $(".bh-inferior.boton-edit-calzado").addClass("hide");
+  });
+  $(".bl-derecha.boton-edit-calzado").click(function(){
+    $(".bl-derecha").removeClass("hide");
+    $(".bl-derecha.boton-edit-calzado").addClass("hide");
+  });
+
+  //desactiva los cuadros de opciones de crea tu calzado
+  $(".cerrar-bl-i").click(function(){
+    $(".bl-izquierda").addClass("hide");
+    $(".bl-izquierda.boton-edit-calzado").removeClass("hide");
+  });
+  $(".cerrar-bh-i").click(function(){
+    $(".bh-inferior").addClass("hide");
+    $(".bh-inferior.boton-edit-calzado").removeClass("hide");
+  });
+  $(".cerrar-bl-d").click(function(){
+    $(".bl-derecha").addClass("hide");
+    $(".bl-derecha.boton-edit-calzado").removeClass("hide");
+  });
 });
